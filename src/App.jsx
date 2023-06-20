3ff20fc487eb1c0fc16cfe6281c57bbe9f43876d
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-19 21:06:15 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import Die from "./Die"
+import './App.css'
+
+
+export default function App() {
+  const [diceValues, setDiceValues] = useState(allNewDice())
+  
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push(Math.ceil(Math.random() * 6))
+      }
+      return newDice
+  }
+  
+  const diceElements = diceValues.map(die => <Die value={die} />)
+  
+  return (
+      <main>
+          <div className="dice-container">
+              {diceElements}
+          </div>
+      </main>
+  )
+}
+
