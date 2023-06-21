ee4055fe91493dcc572b9a1d6a3c5f9a31d20844
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-06-21 11:34:57 -0400

    Change die div to button

--- src/Die.jsx
+++ src/Die.jsx
@@ -8,7 +8,7 @@ const Die = forwardRef((props, ref) => {
         backgroundColor: props.isHeld ? "#FCBA28" : "white"
     }
     return (
-        <div 
+        <button 
             tabIndex="0"
             aria-label={`Die with a value of ${props.value}. ${props.isHeld ? "Frozen" : "Not frozen"}`}
             className="die-face" 
@@ -17,7 +17,7 @@ const Die = forwardRef((props, ref) => {
             ref={ref}
         >
             <h2 className="die-num">{props.value}</h2>
-        </div>
+        </button>
     )
 })
 
